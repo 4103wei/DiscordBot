7e2b9e4dc9684f9b16f06aee816c2353d2a8b060
Wei <jerryjerry4103@gmail.com>
2019-04-03 17:22:52 +0200

    Added wallpaper function Improved code

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import discord
 import random
 import reddit
 import json
-import threading
 import asyncio
+import wget
 
 # Reading Token
 with open('discordLogin.json', 'r') as f:
@@ -14,27 +14,27 @@ client = discord.Client()
 
 async def bg_task():
     await client.wait_until_ready()
-    latest_news = ""
-    latest_trailer = ""
-    latest_livestreamfail = ""
     channel = discord.Object(id=datastore['chat_channel_id'])
+
+    subs = ['worldnews', 'trailers', 'livestreamfail']
+    last_msg = {}
+    for sub in subs:
+        last_msg[sub] = ''
+
+    initial = 1 # If bot is initialized, he should not send a message
     while not client.is_closed:
-        news = reddit.Reddit.getTopStory(sub='worldnews', lim=1)
-        trailer = reddit.Reddit.getTopStory(sub='trailers', lim=1)
-        livestreamfail = reddit.Reddit.getTopStory(sub='livestreamfail', lim=1)
-        if latest_news != news:
-            await client.send_message(channel, "**News**")
-            latest_news = news
-            await client.send_message(channel, news)
-        if latest_trailer != trailer:
-            await client.send_message(channel, "**Trailer**")
-            latest_trailer = trailer
-            await client.send_message(channel, trailer)
-        if latest_livestreamfail != livestreamfail:
-            await client.send_message(channel, "**LivestreamFail**")
-            latest_livestreamfail = livestreamfail
-            await client.send_message(channel, livestreamfail)
-        await asyncio.sleep(300)
+        for sub in subs:
+            try:
+                new_msg = reddit.Reddit.getTopStory(sub=sub, lim=1)
+            except:
+                print('Something went wrong, fetching from ' + sub)
+            if last_msg[sub] != new_msg:
+                if initial == 0:
+                    await client.send_message(channel, '**' + sub + '**')
+                    await client.send_message(channel, new_msg)
+                last_msg[sub] = new_msg
+                initial = 1
+        await asyncio.sleep(600)
 
 
 @client.event
@@ -45,15 +45,25 @@ async def on_message(message):
     if message.content.startswith('order random'):
         msg = "AYES TO THE LEFT " + str(random.randint(1,1000)) + " NOES TO THE RIGHT " + str(random.randint(1,1000))
         await client.send_message(message.channel, msg)
-    elif message.content.startswith('order unleash'):
-        msg = "ORDAHHHH I SAID ORDAHHHHH"
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('order news'):
-        msg = reddit.Reddit.getTopStory(sub='worldnews')
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('order trailers'):
-        msg = reddit.Reddit.getTopStory(sub='trailers')
-        await client.send_message(message.channel, msg)
+    elif message.content.startswith('order anime wallpaper'):
+        try:
+            url = reddit.Reddit.getRandomStory('animewallpaper')
+            await client.send_message(message.channel, url)
+            file = wget.download(url=url,out='files/')
+            print('Downloaded: ' + file)
+        except:
+            await client.send_message(message.channel, 'DIVISSSSIIION!!! something went wrong, try again.')
+    elif message.content.startswith('order wallpaper'):
+        try:
+            url = reddit.Reddit.getRandomStory('wallpapers')
+            await client.send_message(message.channel, url)
+            file = wget.download(url=url, out='files/')
+            print('Downloaded: ' + file)
+            #await client.send_file(message.channel, file)
+        except:
+            await client.send_message(message.channel, 'DIVISSSSIIION!!! something went wrong, try again.')
+
+
     #elif message.content.startswith('order intensifies'):
     #    await client.send_file(message.channel, 'order.jpg')
 
--- reddit.py
+++ reddit.py
@@ -1,10 +1,17 @@
 #! usr/bin/env python3
 import praw
 import json
+import random
 
 class Reddit():
     @staticmethod
     def getTopStory(sub = "worldnews", lim=5):
+        '''
+        fetch the top rising stories from given subreddit
+        :param sub: subreddit (string)
+        :param lim: int (number of stories)
+        :return: titles together with url as a string
+        '''
         with open('redditLogin.json', 'r') as f:
             datastore = json.load(f)
         reddit = praw.Reddit(client_id=datastore['client_id'],
@@ -15,8 +22,30 @@ class Reddit():
 
         wn = reddit.subreddit(sub)
         hot_subreddit = wn.rising(limit=lim)
-
         titles =""
         for submission in hot_subreddit:
             titles = titles + '`' + submission.title + '`' + "\n" + submission.url + "\n"
         return titles
+
+
+    @staticmethod
+    def getRandomStory(sub = "worldnews"):
+        '''
+        fetch a random story from given subreddit
+        :param sub: subreddit (string)
+        :return: url as string
+        '''
+        with open('redditLogin.json', 'r') as f:
+            datastore = json.load(f)
+        reddit = praw.Reddit(client_id=datastore['client_id'],
+                             client_secret=datastore['client_secret'],
+                             user_agent=datastore['user_agent'],
+                             username=datastore['username'],
+                             password=datastore['password'])
+
+        wn = reddit.subreddit(sub)
+        hot_subreddit = wn.hot(limit=500)
+        r = random.randint(1, 500)
+        for i, post in enumerate(hot_subreddit):
+            if i == r:
+                return post.url
